0fff2f91566ce34d1ce8e217a899d1e9f3484ea1
drizzle <51256898+wuyajundeyouxiang@users.noreply.github.com>
2026-05-13 21:48:33 +0800

    enable automatic typescript compilation

--- script.js
+++
@@ -1,9 +0,0 @@
-// 这是翻译给浏览器听的普通 JS 代码
-var btn = document.getElementById('colorBtn');
-var myColors = ['#ff7675', '#74b9ff', '#55efc4', '#ffeaa7'];
-
-btn.addEventListener('click', function () {
-    var randomColor = myColors[Math.floor(Math.random() * myColors.length)];
-    document.body.style.backgroundColor = randomColor;
-    console.log("当前颜色是：" + randomColor);
-});
